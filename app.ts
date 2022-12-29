024ed9bf564ee2ed8510c72fd45141aeafe67a39
jewishmoses <work@xmoses.com>
2022-12-29 23:29:36 +0200

    added uptime endpoint

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
 import * as dotenv from 'dotenv';
 dotenv.config()
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 const songsRoute = require('./routes/songs');
 app.use('/songs', songsRoute);
 
+app.get('/', async (req: Request, res: Response) => {
+
+    const uptime = process.uptime();
+    res.send(`The process has been running for ${uptime} seconds.`)
+
+})
 export default app;
